6e2b29f602364755de49bef1489512e9d25ae329
Longnthe163480 <longnthe163480@fpt.edu.vn>
2025-04-19 23:32:50 +0700

    addAccount

--- src/java/controller/ViewAllAccount.java
+++ src/java/controller/ViewAllAccount.java
@@ -4,18 +4,21 @@
  */
 package controller;
 
+import dao.*;
+import entity.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  *
- * @author longc
+ * @author okanh
  */
-public class Account extends HttpServlet {
+public class ViewAllAccount extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,18 +32,25 @@ public class Account extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Account</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Account at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        AccountDAO dao=new AccountDAO();
+        String index=request.getParameter("index");
+        if(index==null) index="1";
+        int indexp=Integer.parseInt(index);
+        
+        
+        List<Account> list=dao.pagingAccount(indexp);
+        
+        
+        int total=dao.getTotalAccount();
+        int end=total/4;
+        if(total%4!=0) end++;
+        
+        List<Role> role=dao.getRole();
+        
+        request.setAttribute("endpage", end);
+        request.setAttribute("allaccount", list);
+        request.setAttribute("role", role);
+        request.getRequestDispatcher("AccountList.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
