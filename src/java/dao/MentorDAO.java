a38b6a7a183809c1ec344c332b47dd4d197f654f
HuyTTQQ <huytqhe171308@fpt.edu.vn>
2025-04-20 21:57:47 +0700

    add MentorInformation

--- src/java/dao/MentorDAO.java
+++ src/java/dao/MentorDAO.java
@@ -146,5 +146,79 @@ public class MentorDAO extends DBContext {
     }
 
 
+    public List<Skill> getSkillAMentor(int mentorid) {
+        List<Skill> skill = new ArrayList<>();
+        query = "SELECT s.id,s.name FROM mentor m, mentorskill ms, skill s\n"
+                + "WHERE m.id=ms.mentorid AND s.id=ms.skillid AND m.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, mentorid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                skill.add(new Skill(id, name));
+            }
+        } catch (Exception e) {
+        }
+        return skill;
+    }
+
+    public List<Job> getJobAMentor(int mentorid) {
+        List<Job> job = new ArrayList<>();
+        query = "SELECT j.id,j.jobname FROM mentor m, mentorjob mj,job j\n"
+                + "WHERE m.id=mj.mentorid AND j.id=mj.jobid AND m.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, mentorid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String jobname = rs.getString("jobname");
+                job.add(new Job(id, jobname));
+            }
+        } catch (Exception e) {
+        }
+        return job;
+    }
     
+    public float getRateAMentor(int mentorid) {
+        query = "WITH t AS(SELECT mc.mentorid id,AVG(CAST (f.star AS FLOAT(2))) averageStar FROM Feedback f, feedbackanswer fa, answer a, mentorcoderequest mc\n"
+                + "WHERE f.id=fa.feedbackid and fa.answerid=a.id and a.mentorcoderequestid=mc.id\n"
+                + "GROUP BY mc.mentorid)\n"
+                + "SELECT t.averageStar\n"
+                + "FROM mentor m,t WHERE m.id=t.id AND m.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, mentorid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getFloat("averageStar");
+            }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    public List<MenteeFeedback> getFeedbackAMentor(int mentorid) {
+        List<MenteeFeedback> list = new ArrayList<>();
+        query = "SELECT TOP 3 f.id,f.comment,me.avatar,me.name FROM Feedback f, feedbackanswer fa"
+                + ", answer a, mentorcoderequest mc,mentee me\n"
+                + "WHERE f.id=fa.feedbackid AND fa.answerid=a.id AND a.mentorcoderequestid=mc.id "
+                + "AND f.menteeid=me.id AND mc.mentorid=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, mentorid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String comment = rs.getString("comment");
+                String avatar = rs.getString("avatar");
+                String name = rs.getString("name");
+                list.add(new MenteeFeedback(id, comment, avatar, name));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
 }
---
+++ src/java/entity/Job.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Job {
+    private int id;
+    private String jobname;
+
+    public Job() {
+    }
+
+    public Job(int id, String jobname) {
+        this.id = id;
+        this.jobname = jobname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJobname() {
+        return jobname;
+    }
+
+    public void setJobname(String jobname) {
+        this.jobname = jobname;
+    }
+}
