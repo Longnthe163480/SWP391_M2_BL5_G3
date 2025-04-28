0b2bd069929a3596d4452ab2c6b734667c591a0c
HuyTTQQ <huytqhe171308@fpt.edu.vn>
2025-04-28 09:44:50 +0700

    Add ViewPostDetail

---
+++ src/java/entity/PostLike.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PostLike {
+    private int id;
+    private int postId;
+    private int accountId;
+    private Date createdDate;
+
+    public PostLike() {
+    }
+
+    public PostLike(int id, int postId, int accountId, Date createdDate) {
+        this.id = id;
+        this.postId = postId;
+        this.accountId = accountId;
+        this.createdDate = createdDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
